04fe386be8465b12d57fe8005365a713c17985c6
lily.khadarina <litater@rambler.ru>
2025-02-06 21:15:09 +0300

    Правки после ревью

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -15,7 +15,6 @@ import ru.yandex.practicum.filmorate.validation.Update;
 
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
-import java.util.Set;
 
 /**
  * Film.
@@ -44,7 +43,7 @@ public class Film {
     @NotNull(groups = {Create.class, Update.class})
     @Positive(groups = {Create.class, Update.class})
     private int duration;
-    private Set<Genre> genres = new LinkedHashSet<>();
+    private LinkedHashSet<Genre> genres = new LinkedHashSet<>();
     @NotNull(groups = {Create.class, Update.class})
     private Mpaa mpa;
 
